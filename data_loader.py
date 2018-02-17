eded17c3d60acabf6ed622320a65f3d675c2b3ca
kentsyx <kentsyx@gmail.com>
2018-02-17 15:22:50 +0800

    a custom AVA dataset

---
+++ data_loader.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8
+
+from __future__ import absolute_import
+
+import os
+
+import pandas as pd
+from PIL import Image
+
+import torch
+from torch.utils import data
+
+class AVADataset(data.Dataset):
+    """AVA dataset
+    
+    Args: 
+        csv_file: a 11-column csv_file, column one contains the names of image files, column 2-11 contains the empiricial distributions of ratings
+        root_dir: directory to the images
+        transform: prepeocessing and augmentation of the training images
+    """
+
+    def __init__(self, csv_file, root_dir, transform=None):
+        self.annotations = pd.read_csv(csv_file, index_col=0)
+        self.root_dir = root_dir
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.annotations)
+
+    def __getitem__(self, idx):
+        img_name = os.path.join(self.root_dir, str(self.annotations.iloc[idx, 0]) + '.jpg')
+        image = Image.open(img_name)
+        annotations = self.annotations.iloc[idx, 1:].as_matrix()
+        annotations = annotations.astype('float').reshape(-1, 1)
+        sample = {'img_id': img_name, 'image': image, 'annotations': annotations}
+
+        if self.transform:
+            sample['image'] = self.transform(sample['image'])
+
+        return sample
