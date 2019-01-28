0277f2a55bda88cf655bfff935aae39e08a1828b
kentsyx <kentsyx@gmail.com>
2019-01-28 07:10:15 -0500

    minor fix

--- main.py
+++ main.py
@@ -193,7 +193,7 @@ def main(config):
             for i, elem in enumerate(output, 1):
                 predicted_mean += i * elem
             for j, elem in enumerate(output, 1):
-                predicted_std += elem * (i - predicted_mean) ** 2
+                predicted_std += elem * (j - predicted_mean) ** 2
             mean_preds.append(predicted_mean)
             std_preds.append(predicted_std)
         # Do what you want with predicted and std...
