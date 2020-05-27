change to dump prediction; fix error in std computation

test.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 import pandas as pd
+from tqdm import tqdm
 import torch
 import torchvision.models as models
 import torchvision.transforms as transforms
@@ -16,7 +17,7 @@ parser.add_argument('--test_csv', type=str, help='test csv file')
 parser.add_argument('--test_images', type=str, help='path to folder containing images')
 parser.add_argument('--out', type=str, help='dest for images with predicted score')
 parser.add_argument('--workers', type=int, default=4, help='number of workers')
-parser.add_argument('--vis', action='store_true', help='visualization')
+parser.add_argument('--predictions', type=str, help='output file to store predictions')
 args = parser.parse_args()
 
 if not os.path.exists(args.out):
@@ -31,6 +32,9 @@ try:
 except:
     raise
 
+seed = 42
+torch.manual_seed(seed)
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 model = model.to(device)
@@ -43,14 +47,14 @@ test_transform = transforms.Compose([
     transforms.ToTensor()
     ])
 
-
-test_imgs = [f for f in os.listdir(args.test_images)]
-
 test_df = pd.read_csv(args.test_csv, header=None)
+test_imgs = test_df[0]
+pbar = tqdm(total=len(test_imgs))
 
 mean, std = 0.0, 0.0
 for i, img in enumerate(test_imgs):
-    im = Image.open(os.path.join(args.test_images, img))
+    im = Image.open(os.path.join(args.test_images, str(img) + '.jpg'))
+    im = im.convert('RGB')
     imt = test_transform(im)
     imt = imt.unsqueeze(dim=0)
     imt = imt.to(device)
@@ -60,18 +64,19 @@ for i, img in enumerate(test_imgs):
     for j, e in enumerate(out, 1):
         mean += j * e
     for k, e in enumerate(out, 1):
-        std += (e * (k - mean) ** 2) ** (0.5)
-    gt = test_df[test_df[0] == int(img.split('.')[0])].to_numpy()[:, 1:].reshape(10, 1)
+        std += e * (k - mean) ** 2
+    std = std ** 0.5
+    gt = test_df[test_df[0] == img].to_numpy()[:, 1:].reshape(10, 1)
     gt_mean = 0.0
     for l, e in enumerate(gt, 1):
         gt_mean += l * e
-    print(img.split('.')[0] + ' mean: %.3f | std: %.3f | GT: %.3f' % (mean, std, gt_mean))
-    if args.vis:
-        plt.imshow(im)
-        plt.axis('off')
-        plt.title('%.3f (%.3f)' % (mean, gt_mean))
-        plt.savefig(os.path.join(args.out, img.split('.')[0] + '_predicted.png'))
+    # print(str(img) + ' mean: %.3f | std: %.3f | GT: %.3f' % (mean, std, gt_mean))
+    if not os.path.exists(args.predictions):
+        os.makedirs(args.predictions)
+    with open(os.path.join(args.predictions, 'pred.txt'), 'a') as f:
+        f.write(str(img) + ' mean: %.3f | std: %.3f | GT: %.3f\n' % (mean, std, gt_mean))
     mean, std = 0.0, 0.0
+    pbar.update()
 
 
 